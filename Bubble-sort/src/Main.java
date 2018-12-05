d12abe5d2c380678dbcf260b305b6dd69a9e2403
coderavi13 <ravinivangune13@gmail.com>
2018-12-05 23:03:10 +0530

    changing variable name

--- Bubble-sort/src/Main.java
+++ Bubble-sort/src/Main.java
@@ -3,34 +3,32 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		boolean flag = false;
+		boolean isSorted = false;
 		// int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 		// int[] intArray = { 1,2,3,4,5,6,7 };
 		// int[] intArray = { 7,6,5,4,3,2,1 };
-		int[] intArray = { 1, 2, 3, 4, 7, 6, 5 };
+		int[] intArray = { 1, 2, 3, 4, 5, 6, 5 };
 		int count = 1;
 
 		for (int j = 1; j <= intArray.length; j++) {
 			for (int i = 0; i < intArray.length - j; i++) {
 				count++;
-				flag = true;
+				isSorted = true;
 				if (intArray[i] > intArray[i + 1]) {
-
 					int temp = intArray[i];
 					intArray[i] = intArray[i + 1];
 					intArray[i + 1] = temp;
-					flag = false;
+					isSorted = false;
 				}
 			}
-			if (flag)
+			if (isSorted)
 				break;
-
 		}
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.println(intArray[i]);
 		}
 
-		System.out.println("Count=" + count);
+		System.out.println("Number of iterations=" + count);
 
 	}
 
