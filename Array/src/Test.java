a1d94beff37294cdb3a3936c3a427e0c2ea17101
coderavi13 <ravinivangune13@gmail.com>
2018-12-05 22:00:12 +0530

    Data structure first commit

---
+++ Array/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray =new int[5];
+		intArray[0] = 1;
+		intArray[1] = 45;
+		intArray[2] = -54;
+		intArray[3] = 2;
+		intArray[4] = 87;
+		
+		for(int i : intArray) {
+			System.out.println(i);
+		}
+
+	}
+
+}
