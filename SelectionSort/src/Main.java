29a14759901281c9eccc78c6b56609c6085b7585
coderavi13 <ravinivangune13@gmail.com>
2018-12-06 09:39:40 +0530

    Added Selection sort alternative code

---
+++ SelectionSort/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+int swappCount=0;
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
+                lastUnsortedIndex--) {
+
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swappCount++;
+            swap(intArray, largest, lastUnsortedIndex);
+
+        }
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+        
+        System.out.println("Swap count " +swappCount);
+
+
+    }
+
+    public static void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+}
--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -1,4 +1,4 @@
-
+// Owner : Ravi
 public class SelectionSort {
 
 	public static void main(String[] args) {
@@ -6,39 +6,47 @@ public class SelectionSort {
 		// int[] intArray= {1,2,3,4,5,6,7};
 		// int[] intArray ={7,6,5,4,3,2,1};
 		// int[] intArray = { 1, 2, 3, 4, 5, 6, 5 };
-		int[] intArray = { 8, 7, 6, 1, 2, 3, 4 };
+		// int[] intArray = { 8, 7, 6, 1, 2, 3, 4 };
+
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 		int j;
 		int swapCount = 0;
 		int indexOfLargest;
 		boolean isSorted = false;
+		
 		for (int i = 0; i < intArray.length; i++) {
+			
 			indexOfLargest = 0;
+			
 			for (j = 1; j < intArray.length - i; j++) {
 
 				isSorted = true;
+				
 				if (intArray[indexOfLargest] < intArray[j]) {
 					indexOfLargest = j;
 				}
 			}
 
 			j--;
+			
 			if (indexOfLargest != j) {
+				
 				int temp = intArray[indexOfLargest];
 				intArray[indexOfLargest] = intArray[j];
 				intArray[j] = temp;
+				
 				swapCount++;
 				isSorted = false;
+			}
 
-			}
-			for (int element : intArray) {
-				System.out.print(" " + element + " ");
-			}
 			if (isSorted)
 				break;
-			System.out.println("**" + i + "\n");
+		}
+		for (int element : intArray) {
+			System.out.print(" " + element + " ");
 		}
 
-		System.out.println("Swapp Count " + swapCount);
+		System.out.println("\nSwapp Count " + swapCount);
 
 	}
 
