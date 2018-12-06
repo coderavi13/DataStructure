701ff201b9d7404583fa4ba19b18aad1d509e45d
coderavi13 <ravinivangune13@gmail.com>
2018-12-06 23:07:27 +0530

    Optimizing insertion sort

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -9,29 +9,21 @@ public class InsertionSort {
 		// int[] intArray = { 8, 7, 6, 1, 2, 3, 4 };
 
 		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-		
+
 		int pivot;
 
 		for (int i = 1; i < intArray.length; i++) {
-			
+
 			pivot = intArray[i];
-			
-			for (int j = i - 1; j >= 0; j--) {
-				
-				if (pivot < intArray[j]) {
-					
-					intArray[j + 1] = intArray[j];
-				} 
-				else {
-					
-					intArray[j + 1] = pivot;
-					break;
-				}
-				if (j == 0) {
-					
-					intArray[j] = pivot;
-				}
+			int j;
+
+			for (j = i; j > 0 && pivot < intArray[j-1]; j--) {
+
+				intArray[j] = intArray[j-1];
 			}
+
+			intArray[j] = pivot;
+
 		}
 		for (int k : intArray) {
 			System.out.print(" " + k + " ");
